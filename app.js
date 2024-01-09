d5c3ebb48462f6ced44cc6c2bd9f575b8e76721c
Tom <tom@atomic82.com>
2024-01-09 07:02:57 -0500

    Import font, add CSS color variables, minor changes

--- app.js
+++ app.js
@@ -1,2 +1,48 @@
+// Constants
 
-console.log('sanity')
+// Variables (state)
+
+// Cached Element References
+
+let displayElement = document.getElementById("math-result");
+
+let inputElement1 = document.getElementById("math-input-1");
+let inputElement2 = document.getElementById("math-input-2");
+
+let plusBtn = document.querySelector("#plus");
+let minusBtn = document.querySelector("#minus");
+let timesBtn = document.querySelector("#times");
+let dividedBtn = document.querySelector("#divided");
+
+// Event listeners
+
+// Operator button functions
+plusBtn.addEventListener("click", function () {
+  let result = parseInt(inputElement1.value) + parseInt(inputElement1.value);
+  displayElement.textContent = result;
+});
+
+minusBtn.addEventListener("click", function () {
+  let result = parseInt(inputElement1.value) - parseInt(inputElement1.value);
+  displayElement.textContent = result;
+});
+
+timesBtn.addEventListener("click", function () {
+  let result = parseInt(inputElement1.value) * parseInt(inputElement1.value);
+  displayElement.textContent = result;
+});
+dividedBtn.addEventListener("click", function () {
+  let result = parseInt(inputElement1.value) / parseInt(inputElement1.value);
+  displayElement.textContent = result;
+});
+
+// equals
+// percent
+// sign 
+// backspace/delete
+// clear
+// add decimal
+
+
+
+// Functions
